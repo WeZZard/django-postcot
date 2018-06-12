b3938c1ae1537983ffbe4ebb9a19f21ff52b9326
WeZZard <WeZZardLau@gmail.com>
2018-06-13 07:42:24 +0800

    Adds index to the primary site.

--- primary/primary/urls.py
+++ primary/primary/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.defaults import permission_denied
 
 urlpatterns = [
+    path('', permission_denied, {'exception': 'None.'}, name='index'),
     path('admin/', admin.site.urls),
+    path('admin/doc/', include('django.contrib.admindocs.urls')),
 ]
