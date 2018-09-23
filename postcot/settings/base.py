38bbf42b57239b047575c3e59d1e5c033e13f830
WeZZard <WeZZardLau@gmail.com>
2018-09-23 23:29:24 +0800

    Adds static root and media root.

--- postcot/settings/base.py
+++ postcot/settings/base.py
@@ -111,3 +111,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
