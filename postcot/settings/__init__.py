417ec068c51da14fbbf7ec5e3bec5dbb4eea2864
WeZZard <WeZZardLau@gmail.com>
2019-02-24 03:35:39 +0800

    Adjusts settings for Heroku.

--- postcot/settings/__init__.py
+++ postcot/settings/__init__.py
@@ -1,5 +1,1 @@
 from .base import *
-
-import django_heroku
-django_heroku.settings(locals())
-TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
---
+++ postcot/settings/heroku.py
@@ -0,0 +1,6 @@
+from .base import *
+
+import django_heroku
+
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
