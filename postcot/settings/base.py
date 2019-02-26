348bab3093e7371ecd67d44fa0ed2ac8ec7a5b43
WeZZard <WeZZardLau@gmail.com>
2019-02-27 06:07:49 +0800

    Updates settings.

--- postcot/settings/base.py
+++ postcot/settings/base.py
@@ -115,3 +115,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
